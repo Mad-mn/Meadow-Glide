0bb8dbd8605e7b15abd635ca0b4c78a1582eab5f
DevVitalii <dev.vitalii@retrostylegames.com>
2026-06-08 16:23:37 +0300

    Install tiny sauce sdk

--- Assets/Feature/LevelModule/Scripts/LevelService.cs
+++ Assets/Feature/LevelModule/Scripts/LevelService.cs
@@ -13,6 +13,8 @@ namespace Feature.LevelModule.Scripts {
         private readonly LevelModel _levelModel;
         private LevelConfigProvider _levelConfigProvider;
 
+        private int _startedLevelId;
+
         public LevelService(UniTask<LevelConfigProvider> levelConfigProviderTask, ISaveDataModel saveDataModel, ITutorialService tutorialService,
             ISegmentStatusService segmentStatusService, LevelModel levelModel) {
             _levelConfigProviderTask = levelConfigProviderTask;
@@ -35,10 +37,16 @@ namespace Feature.LevelModule.Scripts {
             _tutorialService.TryActivateTutorial();
             _segmentStatusService.UpdateStatus();
             _levelModel.StartLevel();
+            _startedLevelId = GetLevelDataForCurrentLevel()
+                .LevelID;
+            TinySauce.OnGameStarted(_startedLevelId);
         }
 
         public void LevelEnded() {
             _levelModel.EndLevel();
+            bool isUserCompleteLvl = GetLevelDataForCurrentLevel()
+                .LevelID > _startedLevelId;
+            TinySauce.OnGameFinished(isUserCompleteLvl, 0);
         }
     }
 }
