f2fa92da87ba1e16a07d1d1a73bbefe9f3fcfc8f
DevVitalii <dev.vitalii@retrostylegames.com>
2026-06-28 14:33:18 +0300

    Finish windows ui update

--- Assets/Feature/DailyChallengeStartViewModule/Scripts/DailyChallengeStartView.cs
+++ Assets/Feature/DailyChallengeStartViewModule/Scripts/DailyChallengeStartView.cs
@@ -10,15 +10,11 @@ namespace Feature.DailyChallengeStartViewModule.Scripts {
         [SerializeField] private TMP_Text _maxMovesText;
         [SerializeField] private Button _playButton;
         [SerializeField] private Button _closeButton;
-        [SerializeField] private GameObject _lockIcon;
-        [SerializeField] private GameObject _lockText;
 
         public ChallengeMilestone[] Milestones => _milestones;
         public TMP_Text TimerText => _timerText;
         public TMP_Text MaxMovesText => _maxMovesText;
         public Button PlayButton => _playButton;
         public Button CloseButton => _closeButton;
-        public GameObject LockIcon => _lockIcon;
-        public GameObject LockText => _lockText;
     }
 }
--- Assets/Feature/PerfectMapViewModule/Scripts/LevelInfoPerfectChallenge.cs
+++ Assets/Feature/PerfectMapViewModule/Scripts/LevelInfoPerfectChallenge.cs
@@ -1,5 +1,6 @@
 using Feature.LocalizationModule.Scripts;
 using Feature.LocalizationModule.Scripts.Data;
+using Feature.PerfectMapViewModule.Scripts;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,27 +17,30 @@ public class LevelInfoPerfectChallenge : MonoBehaviour {
     [SerializeField] private GameObject _claimedTxt;
 
     private ILocalizationService _localizationService;
-    public Button PlayButton => _playButton;
-    public Button ClaimButton => _claimButton;
+    public Button PlayButton =>
+        _playButton;
+    public Button ClaimButton =>
+        _claimButton;
 
     [Inject]
     private void Construct(ILocalizationService localizationService) {
         _localizationService = localizationService;
     }
 
-    public void Setup(Feature.PerfectMapViewModule.Scripts.LevelInfoPerfectData data) {
+    public void Setup(LevelInfoPerfectData data) {
         _levelNumberText.text = $"{_localizationService.Get(LocalizationKey.Global_Level)}: {data.LevelNumber.ToString()}";
         _bestResult.text = $"{_localizationService.Get(LocalizationKey.PerfectChallenge_YouResult)}: {data.BestMoves.ToString()}";
         _perfectResult.text = $"{_localizationService.Get(LocalizationKey.PerfectChallenge_Perfect)}: {data.ShortestSolution.ToString()}";
 
-        _perfectIcon.SetActive(data.State != Feature.PerfectMapViewModule.Scripts.LevelPerfectState.NotCompleted);
-        _playButton.gameObject.SetActive(data.State == Feature.PerfectMapViewModule.Scripts.LevelPerfectState.NotCompleted
-            || data.State == Feature.PerfectMapViewModule.Scripts.LevelPerfectState.CompletedNotPerfect);
-        _claimButton.gameObject.SetActive(data.State == Feature.PerfectMapViewModule.Scripts.LevelPerfectState.PerfectNotClaimed);
-        _claimedTxt.SetActive(data.State == Feature.PerfectMapViewModule.Scripts.LevelPerfectState.PerfectClaimed);
-        _rewardText.gameObject.SetActive(data.State == Feature.PerfectMapViewModule.Scripts.LevelPerfectState.PerfectNotClaimed);
+        _perfectIcon.SetActive(data.State == LevelPerfectState.PerfectClaimed  || data.State == LevelPerfectState.PerfectNotClaimed);
+        _playButton.gameObject.SetActive(data.State == LevelPerfectState.NotCompleted ||
+                                         data.State == LevelPerfectState.CompletedNotPerfect);
 
-        if (data.State == Feature.PerfectMapViewModule.Scripts.LevelPerfectState.PerfectNotClaimed) {
+        _claimButton.gameObject.SetActive(data.State ==LevelPerfectState.PerfectNotClaimed);
+        _claimedTxt.SetActive(data.State == LevelPerfectState.PerfectClaimed);
+        _rewardText.gameObject.SetActive(data.State == LevelPerfectState.PerfectNotClaimed);
+
+        if (data.State == LevelPerfectState.PerfectNotClaimed) {
             _rewardText.text = $"+{data.RewardAmount}";
         }
     }
